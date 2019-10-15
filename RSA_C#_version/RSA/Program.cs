7e389c2ef2833ffabd021911d1f0043d637be2f1
Anton Podchuvalov <podchuvalovvl@gmail.com>
2019-10-15 22:00:27 +1000

    C# RSA: first implementation

--- RSA_C#_version/RSA/Program.cs
+++ RSA_C#_version/RSA/Program.cs
@@ -7,6 +7,20 @@ namespace RSA
     {
         static void Main(string[] args)
         {
+            RSA.GenerateKeys(out RSAKey openKey, out RSAKey closeKey);
+
+            Console.WriteLine($"Open key:  {{{openKey.Key}, {openKey.Module} }}");
+            Console.WriteLine($"Close key: {{{closeKey.Key}, {closeKey.Module} }}");
+
+            // Example from https://ru.wikipedia.org/wiki/RSA.
+            BigInteger message = 111111;
+            BigInteger code = RSA.Encrypt(message, openKey);
+            BigInteger decryptMessage = RSA.Decrypt(code, closeKey);
+
+            Console.WriteLine($"Message: {message}");
+            Console.WriteLine($"Code:    {code}");
+            Console.WriteLine($"Decrypt: {decryptMessage}");
+            Console.ReadKey();
         }
     }
 }
---
+++ RSA_C#_version/RSA/RSA.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Numerics;
+
+namespace RSA
+{
+    public class RSAKey
+    {
+        public RSAKey(BigInteger key, BigInteger module)
+        {
+            Key = key;
+            Module = module;
+        }
+
+        public BigInteger Key { get; private set; }
+        public BigInteger Module { get; private set; }
+    }
+
+    class RSA
+    {
+        static public void GenerateKeys(out RSAKey openKey, out RSAKey closeKey)
+        {
+            // Example from https://ru.wikipedia.org/wiki/RSA.
+            BigInteger p = 3557;
+            BigInteger q = 2579;
+            BigInteger n = p * q;
+            BigInteger eulerResult = (p - 1) * (q - 1);
+            // For open key used fifth number Fermat.
+            BigInteger e = 65537;
+            BigInteger d = EuclidExtendedModified(eulerResult, e);
+
+            openKey = new RSAKey(e, n);
+            closeKey = new RSAKey(d, n);
+        }
+
+        static public BigInteger Encrypt(BigInteger cipher, RSAKey openKey)
+        {
+            return BigInteger.ModPow(cipher, openKey.Key, openKey.Module);
+        }
+
+        static public BigInteger Decrypt(BigInteger cipher, RSAKey closeKey)
+        {
+            return BigInteger.ModPow(cipher, closeKey.Key, closeKey.Module);
+        }
+
+        static private BigInteger EuclidExtendedModified(BigInteger eulerResult, BigInteger openKey)
+        {
+            BigInteger a = eulerResult;
+            BigInteger b = openKey;
+            BigInteger c = eulerResult;
+            BigInteger d = 1;
+            while (b != 1)
+            {
+                BigInteger g = a / b;
+                BigInteger h = g * d;
+                BigInteger i = a - b * g;
+                if (i < 0)
+                {
+                    i = (i / eulerResult + 1) * eulerResult + i;
+                }
+                BigInteger j = c - h;
+                if (j < 0)
+                {
+                    j = (j / eulerResult + 1) * eulerResult + j;
+                }
+                a = b;
+                b = i;
+                c = d;
+                d = j;
+            }
+            return d;
+        }
+    }
+}
