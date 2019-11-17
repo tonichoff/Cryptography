af08752ff0a40fb2f16618599ff1833221c74fdf
Anton Podchuvalov <podchuvalovvl@gmail.com>
2019-11-17 22:52:32 +1000

    Start implementaion of ElGamal signature

---
+++ ElGamal-Signature/ElGamal-Signature/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ElGamal_Signature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
